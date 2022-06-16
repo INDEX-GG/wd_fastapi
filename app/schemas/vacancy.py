4ba68b9d02c5d25487667d72246f0d1237653686
444karat <38491980+444karat@users.noreply.github.com>
2022-06-16 13:08:21 +0500

    схема вакансий

---
+++ app/schemas/vacancy.py
@@ -0,0 +1,26 @@
+from typing import List
+from pydantic import BaseModel
+
+class VacancyBase(BaseModel):
+    user_id : int
+    title: str
+    description: str | None=None
+    budget: int | None = None
+    name: str | None = None
+    email: str | None = None
+    phone: int | None = None
+    files: List[str] | None= None
+
+#из БД
+class Vacancy (VacancyBase):
+    id: int
+
+    class Config:
+        orm_mode = True
+
+class Vacancies (BaseModel):
+    vacancies : List[Vacancy]
+    vacanciesCount : int | None=None
+
+    class Config:
+        orm_mode : True
